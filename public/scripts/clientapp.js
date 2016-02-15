a6501590dafeaea906c47c32b5b3cb1213524648
andyfehrenbach <Andy Fehrenbach@Andy-Fehrenbachs-MacBook-Pro-2.local>
2016-02-14 19:54:15 -0600

    added number buttons.Everything operates in the calculator's display field. fully working but no CSS

--- public/scripts/clientapp.js
+++ public/scripts/clientapp.js
@@ -1,7 +1,9 @@
 var values = {};
+var count = 0;
 
 $(document).ready(function(){
-    $('#calculate').on('click', getXAndY);
+    $('.num').on('click', calculatorIntake2);
+    $('#calculate').on('click', sendData);
     $('#add').on('click', addOperator);
     $('#subtract').on('click', subtractOperator);
     $('#multiply').on('click', multiplyOperator);
@@ -9,54 +11,72 @@ $(document).ready(function(){
     $('#clear').on('click', clearEverything);
 });
 
-function getXAndY() {
-    event.preventDefault();
-
-    $.each($('.digit').serializeArray(), function(i, field) {
-        values[field.name] = field.value;
-    });
-
-
-
-    console.log(values);
-    $.ajax({
-        type: 'POST',
-        url: '/object',
-        data: values,
-        beforeSend: function() {
-            console.log('before!' + values.type + values.xDigit + values.yDigit);
-        },
-        success: function(data) {
-            refreshDom(data);
-            console.log('From Server: ', data);
-        }
-    });
+//experiment #4
+function calculatorIntake2 () {
+    if (count == 0) {
+        values.xDigit = $(this).val();
+        $('#display').val($(this).val());
+        console.log(values);
+        count++;
+        console.log(count);
+    } else {
+        values.yDigit = $(this).val();
+        $('#display').val($(this).val());
+        console.log(values);
+        count = 0;
+        console.log(count);
+    }
 }
 
 // operator button object assignments
 function addOperator (){
     values.type = 'add';
+    clearDisplay();
 }
 
 function subtractOperator (){
     values.type = 'subtract';
+    clearDisplay();
 }
 
 function multiplyOperator (){
     values.type = 'multiply';
+    clearDisplay();
 }
 
 function divideOperator (){
     values.type = 'divide';
+    clearDisplay();
 }
 
 
 //append data to the dom
 function refreshDom (num) {
-    $('#result').text(num);
+    clearDisplay();
+    $('.digit').find('input').val(num);
 }
 
 function clearEverything (){
     $('.digit').find('input').val('');
     $('#result').text('');
+}
+
+function clearDisplay () {
+    $('.digit').find('input').val('');
+}
+
+function sendData () {
+
+    $.ajax({
+        type: 'POST',
+        url: '/object',
+        data: values,
+        beforeSend: function () {
+            console.log('before!' + values.type + values.xDigit + values.yDigit);
+        },
+        success: function (data) {
+            refreshDom(data);
+            console.log('From Server: ', data);
+        }
+    });
 }
